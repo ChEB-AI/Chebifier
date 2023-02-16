d6d95c6f0014b579a2e91a30a68cb79bdc2c1fc1
MGlauer <martinglauer89@gmail.com>
2023-02-16 07:12:24 +0100

    Implement draft form

--- backend/api/chemclass.py
+++ backend/api/chemclass.py
@@ -1,9 +1,34 @@
 from flask_restful import Api, Resource, reqparse
+from chebai.models.electra import Electra
+from chebai.preprocessing.reader import ChemDataReader
+from chebai.preprocessing.collate import RaggedCollater
+from chebai.result.molplot import AttentionMolPlot
+from tempfile import NamedTemporaryFile
+from PIL import Image
+import base64
+import io
+import numpy as np
+
+model_kwargs = dict(
+    optimizer_kwargs=dict(lr=1e-4),
+    pretrained_checkpoint="Electra_ChEBI100.ckpt",
+    out_dim=854,
+    config=dict(
+        vocab_size=1400,
+        max_position_embeddings=1800,
+        num_attention_heads=8,
+        num_hidden_layers=6,
+        type_vocab_size=1,
+    )
+)
+
+electra_model = Electra(**model_kwargs)
 
 
 class PredictionApiHandler(Resource):
     def post(self):
         parser = reqparse.RequestParser()
+        plotter = AttentionMolPlot()
         parser.add_argument('type', type=str)
         parser.add_argument('smiles', type=str)
 
@@ -15,6 +40,17 @@ class PredictionApiHandler(Resource):
         request_type = args['type']
         smiles = args['smiles']
 
+        reader = ChemDataReader()
+        collater = RaggedCollater()
+        tokenised_input = electra_model._get_data_and_labels(collater([reader.to_data(dict(features=smiles, labels=None))]), 0)
+        result = electra_model(tokenised_input)
+        atts = np.concatenate([a.detach().numpy() for a in result["attentions"]])
+        d = plotter.draw_attention_molecule(smiles, np.max(np.max(atts, axis=2), axis=1))
+        with NamedTemporaryFile(mode="wt", suffix=".png") as svg1:
+            d.WriteDrawingText(svg1.name)
+            im = Image.open(svg1.name)
+            data = io.BytesIO()
+            im.save(data, "JPEG")
+            encoded_img_data = base64.b64encode(data.getvalue())
+        return {"logits": result["logits"].tolist(), "attention_fig": encoded_img_data.decode('utf-8')}
 
-
-        return smiles
--- react-app/src/smiles-form/smiles-form.js
+++ react-app/src/smiles-form/smiles-form.js
@@ -5,26 +5,30 @@ import axios from 'axios'
 export class SmilesForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', attention_fig: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {    this.setState({value: event.target.value});  }
   handleSubmit(event) {
-    axios.post('/api/classify', { smiles: this.state.value }).then(response => {
-        console.log(response);
-    });
     event.preventDefault();
+    axios.post('/api/classify', { smiles: this.state.value }).then(response => {
+        this.setState({attention_fig:response.data.attention_fig});
+    });
   }
 
+
+
   render() {
+    const Example = ({ data }) => <img src={`data:image/jpeg;base64,${data}`} />
     return (
       <form onSubmit={this.handleSubmit}>        <label>
           Smiles:
           <input type="text" defaultValue={this.state.value} onChange={this.handleChange} />        </label>
         <input type="submit" value="Submit" />
       </form>
+      <Example data={this.state.attention_fig} />
     );
   }
 }
