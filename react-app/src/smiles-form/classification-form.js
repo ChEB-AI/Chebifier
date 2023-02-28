62cc7a1081f4d9f64a8d01cc0520692a0901c47a
MGlauer <martinglauer89@gmail.com>
2023-02-28 16:04:11 +0100

    Improve layout

--- react-app/src/smiles-form/classification-form.js
+++ react-app/src/smiles-form/classification-form.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
@@ -12,204 +13,211 @@ import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
 import StartIcon from '@mui/icons-material/Start';
 import {
-  GridRowModes,
-  DataGrid,
-  GridToolbarContainer,
-  GridActionsCellItem,
+    GridRowModes,
+    DataGrid,
+    GridToolbarContainer,
+    GridActionsCellItem,
 } from '@mui/x-data-grid';
 import {
-  randomId,
+    randomId,
 } from '@mui/x-data-grid-generator';
 
 function EditToolbar(props) {
-  const { setRows, setRowModesModel, rows } = props;
+    const {setRows, setRowModesModel, rows} = props;
 
-  const addRows = ((smiles) => {
-        const ids = smiles.map((s) => randomId());
-        setRows((oldRows) => [...oldRows, ...smiles.map((s, i) => ({id:ids[i], smiles: s, parents: []}))]);
-        return ids
-      }
-  )
+    const addRows = ((smiles) => {
+            const ids = smiles.map((s) => randomId());
+            setRows((oldRows) => [...oldRows, ...smiles.map((s, i) => ({id: ids[i], smiles: s, parents: []}))]);
+            return ids
+        }
+    )
 
-  const handleAdd = () => {
-    const ids = addRows([''])
-    setRowModesModel((oldModel) => ({
-          ...oldModel,
-          ...Object.fromEntries(ids.map(id => [id, {mode: GridRowModes.Edit, fieldToFocus: 'smiles'}])),
+    const handleAdd = () => {
+        const ids = addRows([''])
+        setRowModesModel((oldModel) => ({
+            ...oldModel,
+            ...Object.fromEntries(ids.map(id => [id, {mode: GridRowModes.Edit, fieldToFocus: 'smiles'}])),
         }));
-  };
-
-  const handleUpload = (event) => {
-    event.preventDefault();
-    console.log(event);
-    const reader = new FileReader()
-    reader.onload = async (e) => {
-      addRows(e.target.result.split("\n"))
     };
-    reader.readAsText(event.target.files[0])
-  };
 
-  const handleRun = () => {
-    axios({
-        url: '/api/classify',
-        method: 'post',
-        data: {smiles: rows.map((r) => (r["smiles"]))}
-    }).then(response => {
-        setRows((oldRows) => oldRows.map((row, i) => ({
-          ...row, "direct_parents":response.data.direct_parents[i]})));
-    });
-  };
+    const handleUpload = (event) => {
+        event.preventDefault();
+        console.log(event);
+        const reader = new FileReader()
+        reader.onload = async (e) => {
+            addRows(e.target.result.split("\n"))
+        };
+        reader.readAsText(event.target.files[0])
+    };
 
-  return (
-    <GridToolbarContainer>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleAdd}>
-        Add SMILES
-      </Button>
-          <Button color="primary" startIcon={<FileUploadIcon />} component="label" >
-              Upload file
-              <input
-          accept="text/plain"
-          style={{display: 'none'}}
-          id="file-upload"
-          type="file"
-          onChange={handleUpload}
-      />
-          </Button>
+    const handleRun = () => {
+        axios({
+            url: '/api/classify',
+            method: 'post',
+            data: {smiles: rows.map((r) => (r["smiles"]))}
+        }).then(response => {
+            setRows((oldRows) => oldRows.map((row, i) => ({
+                ...row, "direct_parents": response.data.direct_parents[i]
+            })));
+        });
+    };
 
-      <Button color="primary" startIcon={<StartIcon />} onClick={handleRun}>
-        Predict classes
-      </Button>
-    </GridToolbarContainer>
-  );
+    return (
+        <GridToolbarContainer>
+            <Button color="primary" startIcon={<AddIcon/>} onClick={handleAdd}>
+                Add SMILES
+            </Button>
+            <Button color="primary" startIcon={<FileUploadIcon/>} component="label">
+                Upload file
+                <input
+                    accept="text/plain"
+                    style={{display: 'none'}}
+                    id="file-upload"
+                    type="file"
+                    onChange={handleUpload}
+                />
+            </Button>
+            <Divider/>
+            <Button color="primary" startIcon={<StartIcon/>} onClick={handleRun}>
+                Predict classes
+            </Button>
+        </GridToolbarContainer>
+    );
 }
 
 EditToolbar.propTypes = {
-  setRowModesModel: PropTypes.func.isRequired,
-  setRows: PropTypes.func.isRequired,
+    setRowModesModel: PropTypes.func.isRequired,
+    setRows: PropTypes.func.isRequired,
 };
 
 export default function ClassificationGrid() {
-  const [rows, setRows] = React.useState([]);
-  const [rowModesModel, setRowModesModel] = React.useState({});
+    const [rows, setRows] = React.useState([]);
+    const [rowModesModel, setRowModesModel] = React.useState({});
 
-  const handleRowEditStart = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
+    const handleRowEditStart = (params, event) => {
+        event.defaultMuiPrevented = true;
+    };
 
-  const handleRowEditStop = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
+    const handleRowEditStop = (params, event) => {
+        event.defaultMuiPrevented = true;
+    };
 
-  const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
+    const handleEditClick = (id) => () => {
+        setRowModesModel({...rowModesModel, [id]: {mode: GridRowModes.Edit}});
+    };
 
-  const handleSaveClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
+    const handleSaveClick = (id) => () => {
+        setRowModesModel({...rowModesModel, [id]: {mode: GridRowModes.View}});
+    };
 
-  const handleDeleteClick = (id) => () => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
+    const handleDeleteClick = (id) => () => {
+        setRows(rows.filter((row) => row.id !== id));
+    };
 
-  const handleCancelClick = (id) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: {mode: GridRowModes.View, ignoreModifications: true},
+        });
 
-    const editedRow = rows.find((row) => row.id === id);
-    if (editedRow.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
-  };
-
-  const processRowUpdate = (newRow) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-    return updatedRow;
-  };
-
-  const columns = [
-    { field: 'smiles', headerName: 'Smiles', flex: 0.45, editable: true },
-    { field: 'direct_parents', headerName: 'Predicted Class', flex: 0.45, editable: false },
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      flex: 0.1,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow.isNew) {
+            setRows(rows.filter((row) => row.id !== id));
         }
+    };
 
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
-  ];
+    const processRowUpdate = (newRow) => {
+        const updatedRow = {...newRow, isNew: false};
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        return updatedRow;
+    };
 
-  return (
-      <Paper sx={{width: "100%"}}>
-    <Box
-      sx={{
-        height: 500,
-        width: '100%',
-        '& .actions': {
-          color: 'text.secondary',
+    const columns = [
+        {field: 'smiles', headerName: 'Smiles', flex: 0.45, editable: true},
+        {field: 'direct_parents', headerName: 'Predicted Class', flex: 0.45, editable: false},
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            flex: 0.1,
+            cellClassName: 'actions',
+            getActions: ({id}) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon/>}
+                            label="Save"
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon/>}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ];
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon/>}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon/>}
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
         },
-        '& .textPrimary': {
-          color: 'text.primary',
-        },
-      }}
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        editMode="row"
-        rowModesModel={rowModesModel}
-        onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
-        onRowEditStart={handleRowEditStart}
-        onRowEditStop={handleRowEditStop}
-        processRowUpdate={processRowUpdate}
-        components={{
-          Toolbar: EditToolbar,
-        }}
-        componentsProps={{
-          toolbar: { setRows, setRowModesModel, rows },
-        }}
-        experimentalFeatures={{ newEditingApi: true }}
-      />
-    </Box>
-        </Paper>
-  );
+    ];
+
+    return (
+        <Box sx={{width: "100%"}}>
+            <Box>
+                <h1>Chebifier</h1>
+                Classify chemical structures using AI.
+            </Box>
+            <Paper sx={{width: "100%"}}>
+                <Box
+                    sx={{
+                        height: 500,
+                        width: '100%',
+                        '& .actions': {
+                            color: 'text.secondary',
+                        },
+                        '& .textPrimary': {
+                            color: 'text.primary',
+                        },
+                    }}
+                >
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        editMode="row"
+                        rowModesModel={rowModesModel}
+                        onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
+                        onRowEditStart={handleRowEditStart}
+                        onRowEditStop={handleRowEditStop}
+                        processRowUpdate={processRowUpdate}
+                        components={{
+                            Toolbar: EditToolbar,
+                        }}
+                        componentsProps={{
+                            toolbar: {setRows, setRowModesModel, rows},
+                        }}
+                        experimentalFeatures={{newEditingApi: true}}
+                    />
+                </Box>
+            </Paper>
+        </Box>
+    );
 }
