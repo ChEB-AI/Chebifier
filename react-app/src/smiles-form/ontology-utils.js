4a82b240f0a3406a7a53c87fe9d3062d3e3971e4
MGlauer <martinglauer89@gmail.com>
2023-05-11 20:16:21 +0200

    Add toggle for hierarchical view

--- react-app/src/smiles-form/ontology-utils.js
+++ react-app/src/smiles-form/ontology-utils.js
@@ -5,6 +5,9 @@ import {Network} from "vis-network";
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import List from '@mui/material/List';
@@ -20,7 +23,7 @@ function buildNode(id, node, includeLabel=true){
     if(!node.artificial){
         d["lbl"] = d["title"]
     } else {
-        d["color"] = "#EEEEEC"
+        d["color"] = "#c4c4c0"
     }
     return d
 }
@@ -54,8 +57,6 @@ function renderOverview(node, graph){
 
     }
     const nodeDict = Object.fromEntries(graph.nodes.map(x => [x["id"], x]));
-
-    console.log(graph);
     const superclasses = graph.edges.filter((e) => (e["from"] == node)).map((e) => renderClassListElement(e["to"], nodeDict[e["to"]]))
     const subclasses = graph.edges.filter((e) => (e["to"] == node)).map((e) => renderClassListElement(e["from"], nodeDict[e["from"]]))
 
@@ -84,9 +85,54 @@ export function VisNetwork(data) {
     const visJsRef = useRef(null);
     const [selectedNode, setSelectedNode] = React.useState(null);
     const [graph, setGraph] = React.useState(null);
+
+    const [hierarchical, setHierarchical] = React.useState(true);
+
+
     useEffect(() => {
+        var layout = null;
+        var physics = false;
+        if(hierarchical){
+            layout={
+                hierarchical: {
+                    enabled: true,
+                    direction: "LR",
+                    sortMethod: "directed",
+                    levelSeparation: 250,
+                }
+            }
+        } else {
+            layout={
+                hierarchical: false,
+                improvedLayout: false
+            }
+            physics = true;
+        }
+
+        const interaction = {
+            dragNodes:true,
+            dragView: true,
+            hideEdgesOnDrag: false,
+            hideEdgesOnZoom: false,
+            hideNodesOnDrag: false,
+            hover: true,
+            hoverConnectedEdges: true,
+            keyboard: {
+              enabled: false,
+              speed: {x: 10, y: 10, zoom: 0.02},
+              bindToWindow: true,
+              autoFocus: true,
+            },
+            multiselect: false,
+            navigationButtons: false,
+            selectable: true,
+            selectConnectedEdges: true,
+            tooltipDelay: 300,
+            zoomSpeed: 1,
+            zoomView: true
+        }
+
         if(data.graph != null){
-            console.log(data.graph);
             const g = {
                 nodes: Object.keys(data.graph.nodes).map(k => buildNode(k, data.graph.nodes[k])),
                 edges: Object.keys(data.graph.edges).map(k => buildEdge(k, data.graph.edges[k]))
@@ -94,82 +140,42 @@ export function VisNetwork(data) {
             const network =
                 visJsRef.current &&
                 new Network(visJsRef.current, g, {
-                    physics: {enabled: data.physics},
-                    layout: data.layout,
-                    interaction: data.interaction,
+                    physics: {enabled: physics},
+                    layout: layout,
+                    interaction: interaction,
                     width: data.width || "100%",
                     height: data.height || "100%",
                     clickToUse: true
                 });
             network.fit();
             network.on("selectNode", function (params) {
-                console.log(params);
                 setSelectedNode(params.nodes[0] || null);
             });
             setGraph(g)
         }
 
-    }, [visJsRef, data]);
+    }, [visJsRef, data, hierarchical]);
     return <Grid container spacing={2}>
           <Grid item xs={9}>
             <Box><div ref={visJsRef}/></Box>
           </Grid>
-          <Grid item xs={3}>
-            <Box>{renderOverview(selectedNode, graph)}</Box>
+          <Grid item xs={3} style={{maxHeight: data.height || "100%",  overflow: 'auto'}}>
+            <Typography variant="h6">Graph settings</Typography>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox defaultChecked />} onChange={() => setHierarchical(!hierarchical)} label="Hierarchical (disabling this may take a while)" />
+            </FormGroup>
+            <hr />
+            <Typography variant="h6">Node info</Typography>
+            <Box style={{maxWidth: '100%', overflow: 'auto'}}>{renderOverview(selectedNode, graph)}</Box>
           </Grid>
         </Grid>
 };
 
-export function plot_ontology(graph, physics=false, hierarchical=true) {
-    var layout = null;
-    if(hierarchical){
-        layout={
-            hierarchical: {
-                enabled: true,
-                direction: "LR",
-                sortMethod: "directed",
-                levelSeparation: 250,
-            }
-        }
+export function plot_ontology(graph) {
+
+    if(graph){
+        return <VisNetwork graph={graph} height={"400px"}/>
     } else {
-        layout={
-            hierarchical: {
-                enabled: true,
-                direction: "LR",
-                sortMethod: "directed",
-                levelSeparation: 250,
-            }
-        }
-        physics = true;
+        return ;
     }
-
-    var interaction = {
-        dragNodes:true,
-        dragView: true,
-        hideEdgesOnDrag: false,
-        hideEdgesOnZoom: false,
-        hideNodesOnDrag: false,
-        hover: true,
-        hoverConnectedEdges: true,
-        keyboard: {
-          enabled: false,
-          speed: {x: 10, y: 10, zoom: 0.02},
-          bindToWindow: true,
-          autoFocus: true,
-        },
-        multiselect: false,
-        navigationButtons: false,
-        selectable: true,
-        selectConnectedEdges: true,
-        tooltipDelay: 300,
-        zoomSpeed: 1,
-        zoomView: true
-    }
-
-    return <VisNetwork graph={graph} physics={physics}
-                                        layout={layout}
-                                        interaction={interaction}
-                                        height={"400px"}
-                            />
-
 }
